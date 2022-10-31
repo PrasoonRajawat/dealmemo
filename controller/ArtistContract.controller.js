56e48fc8d18eb40fcf35b8bcdae3a3b10a46cb3e
dhirajsarang <107534100+dhirajsarang@users.noreply.github.com>
2022-10-31 14:58:17 +0530

    ....

--- controller/ArtistContract.controller.js
+++ controller/ArtistContract.controller.js
@@ -431,10 +431,10 @@ sap.ui.define([
 
 			loadDepartment: function () { // added by dhiraj on 24/05/2022
 				var artistContractModel = this.getView().getModel("artistContractModel");
-				vendorContractModel.setProperty("/Iniquoamt", "0.00");
-				vendorContractModel.setProperty("/R1quoamt", "0.00");
-				vendorContractModel.setProperty("/R2quoamt", "0.00");
-				vendorContractModel.setProperty("/Finalquoamt", "0.00");
+				artistContractModel.setProperty("/Iniquoamt", "0.00");
+				artistContractModel.setProperty("/R1quoamt", "0.00");
+				artistContractModel.setProperty("/R2quoamt", "0.00");
+				artistContractModel.setProperty("/Finalquoamt", "0.00");
 				artistContractModel.setProperty("/Skiprfpreason", "");
 				artistContractModel.refresh(true);
 				Fragment.load({
