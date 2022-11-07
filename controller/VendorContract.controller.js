bb292d8a1a2c929d61b02f034b22e08c59fd95c6
dhirajsarang <107534100+dhirajsarang@users.noreply.github.com>
2022-11-07 13:35:16 +0530

    changes for duplicate line item

--- controller/VendorContract.controller.js
+++ controller/VendorContract.controller.js
@@ -2216,7 +2216,7 @@ sap.ui.define([
 								obj.flag = "Cr";
 								for (var oInd = 0; oInd < vendorContractDetailInfo.vcPaymentData.length; oInd++) {
 									var vcEpiObj = vendorContractDetailInfo.vcPaymentData[oInd];
-									if (vcEpiObj.Epiid === obj.Epiid && vcEpiObj.Msid === obj.Msid && vcEpiObj.Contver === obj.Contver && obj.Updkz != "I") {  //vcEpiObj.Dueamt === obj.Dueamt  
+									if (vcEpiObj.Epiid === obj.Epiid && vcEpiObj.Msid === obj.Msid && vcEpiObj.Contver === obj.Contver ) {  //vcEpiObj.Dueamt === obj.Dueamt  
 										flagNewEntry = false;																//Changes for negative milestone as suggested by sourabhk
 										break;
 									}
